508bf4a063a5721ce670e6e2b30b264fd9506074
cullenjett@gmail.com <cullenjett@gmail.com>
2017-02-03 21:27:15 -0700

    finish the header via the AppHeader component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
+import AppHeader from './app-header/AppHeader';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App__header">
-          <div className="container-fluid">
-            <a href="/">
-              <img src="https://cars.blinker.com/images/blinker-logo-tag-line.svg" className="App__logo" alt="logo" />
-            </a>
-          </div>
-        </header>
+        <AppHeader />
       </div>
     );
   }
---
+++ src/app-header/AppHeader.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './AppHeader.css'
+
+const AppHeader = () => {
+  return (
+    <header className="AppHeader">
+      <div className="container-fluid AppHeader__nav">
+        <div className="AppHeader__nav--left">
+          <a href="/">
+            <img src="https://cars.blinker.com/images/blinker-logo-tag-line.svg" className="AppHeader__logo" alt="logo" />
+          </a>
+        </div>
+
+        <nav className="AppHeader__nav--right">
+          <ul className="AppHeader__nav-list">
+            <li><a href="#" className="AppHeader__nav-link">Sell</a></li>
+            <li><a href="#" className="AppHeader__nav-link">Buy</a></li>
+            <li><a href="#" className="AppHeader__nav-link">Refinance</a></li>
+            <li><a href="#" className="AppHeader__nav-link active">Shop</a></li>
+            <li><a href="#" className="AppHeader__nav-link">Questions?</a></li>
+            <li><a href="#" className="AppHeader__nav-link">Download</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default AppHeader;

c713fa1d786ee2b0bebb8f0833b436baf7731f76
cullenjett@gmail.com <cullenjett@gmail.com>
2017-02-04 08:24:13 -0700

    override Bootstrap's <a> styles

--- src/listing-card/ListingCard.js
+++ src/listing-card/ListingCard.js
@@ -4,14 +4,16 @@ import './ListingCard.css';
 const ListingCard = () => {
   return (
     <div className="ListingCard">
-      <img src="http://placehold.it/400x300" className="img img-responsive" />
-      <div className="ListingCard__details">
-        <h3 className="ListingCard__title">2010 Ford Focus</h3>
-        <div>
-          <span className="ListingCard__price">$4,900 or $100/mo*</span>
-          <span className="ListingCard__miles">112,200 mi</span>
+      <a href="#">
+        <img src="http://placehold.it/400x300" className="img img-responsive" />
+        <div className="ListingCard__details">
+          <h3 className="ListingCard__title">2010 Ford Focus</h3>
+          <div>
+            <span className="ListingCard__price">$4,900 or $100/mo*</span>
+            <span className="ListingCard__miles">112,200 mi</span>
+          </div>
         </div>
-      </div>
+      </a>
     </div>
   );
 }

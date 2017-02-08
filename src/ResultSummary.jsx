5cf8716f5ad95e07a42f6ebbf1f62695d1b3492d
cullenjett@gmail.com <cullenjett@gmail.com>
2017-02-07 19:11:55 -0700

    small style adjustments

--- src/ResultSummary.jsx
+++ src/ResultSummary.jsx
@@ -5,7 +5,8 @@ const ResultSummary = () => {
     <div className="row">
       <div className="col-xs-12">
         <p style={{
-            fontWeight: 600,
+            color: "#2B3642",
+            fontWeight: 500,
             padding: "2rem",
             margin: 0,
             borderRight: "2px solid #E5E8EB",
